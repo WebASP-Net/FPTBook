fca7d9eac52f6ec93d3f82f9b88d54dba37ea0a7
phihung07 <106146985+phihung07@users.noreply.github.com>
2023-03-03 12:18:53 +0700

    create home page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,32 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using FPTBOOK.Models;
+using FPTBOOK.Data;
+using Microsoft.EntityFrameworkCore;
+using FPTBook.Models;
 
 namespace FPTBOOK.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _db;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
     {
         _logger = logger;
+        _db = db;
     }
-
-    public IActionResult Index()
+    public IActionResult Index(string SearchString = "")
     {
-        return View();
+        if (SearchString != "")
+        {
+            var Products = _db.Products.Include(s => s.Category).Where(x => x.Name.ToUpper().Contains(SearchString.ToUpper()));
+            return View(Products.ToList());
+        }
+    
+        IEnumerable<Product> lstPro = _db.Products.ToList();
+        return View(lstPro);
     }
 
     public IActionResult Privacy()
