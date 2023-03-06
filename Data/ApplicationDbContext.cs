f1732b6c58dc68154ffac01d2689f33d3596fe4e
phihung07 <106146985+phihung07@users.noreply.github.com>
2023-03-06 14:42:32 +0700

    update register

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<WebApp1User> app_users{get;set;}
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderDetail> OrderDetails { get; set; }
 
