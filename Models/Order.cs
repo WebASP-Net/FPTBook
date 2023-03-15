b47d1fe9e30e2589169aaebe0a22e68e4432c3a2
phihung07 <106146985+phihung07@users.noreply.github.com>
2023-03-15 09:58:10 +0700

    update

--- Models/Order.cs
+++ Models/Order.cs
@@ -8,34 +8,25 @@ namespace FPTBook.Models
 		public int order_id { get; set; }
 
 		[Required(ErrorMessage = "Country Empty")]
-		[StringLength(maximumLength:200,ErrorMessage ="Length must be less than 200 characters")]
 		public string country { get; set; }
 
 		[Required(ErrorMessage = "First Name Empty")]
-		[StringLength(maximumLength:20,ErrorMessage ="Length must be less than 20 characters")]
 		public string cus_first_name { get; set; }
 
 		[Required(ErrorMessage = "Last Name Empty")]
-		[StringLength(maximumLength:20,ErrorMessage ="Length must be less than 20 characters")]
 		public string cus_last_name { get; set; }
 
 		[Required(ErrorMessage = "Address Empty")]
-		[StringLength(maximumLength:100,ErrorMessage ="Length must be less than 20 characters")]
 		public string cus_address { get; set; }
 
 		[Required(ErrorMessage = "City Empty")]
-		[StringLength(maximumLength:50,ErrorMessage ="Length must be less than 20 characters")]
 		public string cus_city { get; set; }
 
 		[Required(ErrorMessage = "Phone Empty")]
-		[StringLength(maximumLength:30,ErrorMessage ="Length must be less than 20 characters")]		
 		public string cus_phone { get; set; }
 		
-		[Required(ErrorMessage = "Email Empty")]
-		[StringLength(maximumLength:150,ErrorMessage ="Length must be less than 20 characters")]			
+		[Required(ErrorMessage = "Email Empty")]			
 		public string cus_email { get; set; }
-		
-
 		[DataType(DataType.Date)]
 		public DateTime OrderDate { get; set; }
 		public virtual ICollection<Customer>? Customers { get; set; }
